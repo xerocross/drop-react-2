b04ea544a6021f996fccc815bb2c2719ff2e9c52
Adam Cross <adamfgcross@gmail.com>
2023-06-02 18:46:20 -0400

    add express server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3500;
+const app = express();
+app.use(cors());
+app.use(express.static('build'))
+app.listen(port, () => console.log(`Drop-React-2 listening on port ${port}!`))
