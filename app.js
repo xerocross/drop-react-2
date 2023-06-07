7bd7b4697e04cdcc61abd200c188548faa82b50f
xerocross <adamfgcross@gmail.com>
2023-06-07 10:42:59 -0400

    fix eslint problems in app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const cors = require('cors');
 const port = process.env.PORT || 3500;
 const app = express();
 app.use(cors());
-app.use(express.static('build'))
-app.listen(port, () => console.log(`Drop-React-2 listening on port ${port}!`))
+app.use(express.static('build'));
+app.listen(port, () => console.log(`Drop-React-2 listening on port ${port}!`));
